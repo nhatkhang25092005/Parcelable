40474ac21c9d4503e07857436c9d97c2c1a33758
Zesk <nhatkhang25092005@gmail.com>
2025-07-06 16:03:06 +0700

    Call, Map, Web Feature =)

--- app/src/main/java/com/example/parcelable/DetailActivity.java
+++ app/src/main/java/com/example/parcelable/DetailActivity.java
@@ -1,7 +1,11 @@
 package com.example.parcelable;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,6 +18,8 @@ import androidx.core.view.WindowInsetsCompat;
 public class DetailActivity extends AppCompatActivity {
     ImageView img_detail;
     TextView txt_detail;
+    Button btn_call,btn_map,btn_web;
+    @SuppressLint("QueryPermissionsNeeded")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +27,9 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
         img_detail = findViewById(R.id.img_detail);
         txt_detail = findViewById(R.id.txt_detail);
+        btn_call = findViewById(R.id.btn_call);
+        btn_map = findViewById(R.id.btn_map);
+        btn_web = findViewById(R.id.btn_web);
 
 
 
@@ -38,5 +47,27 @@ public class DetailActivity extends AppCompatActivity {
             editor.apply();
         }
 
-    }
+        //Call Feature =)
+        btn_call.setOnClickListener(v -> {
+            Intent callIntent = new Intent(Intent.ACTION_DIAL);
+            callIntent.setData(Uri.parse("tel:0123456789"));
+            startActivity(callIntent);
+        });
+
+
+        //See map feature (~.~)
+        btn_map.setOnClickListener(v -> {
+            String geoUri = "geo:0,0?q=1600+Amphitheatre+Parkway,+Mountain+View,+California";
+            Intent mapIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(geoUri));
+            mapIntent.setPackage("com.google.android.apps.maps");
+                startActivity(mapIntent);
+        });
+
+        btn_web.setOnClickListener(v -> {
+            Intent webIntent = new Intent(Intent.ACTION_VIEW);
+            webIntent.setData(Uri.parse("https://www.google.com"));
+            startActivity(webIntent);
+        });
+        }
+
 }
